23a78a0aca6cd538ee8719de9b3898fe1666d753
장원 <jangwon_s@naver.com>
2024-09-02 23:26:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+function Article(props) {
+  return(
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+function Header(props) {
+  return(
+    <header>
+      <h1><a href="/" onClick={(event)=>{
+        event.preventDefault(); //기본동작 방지 > 클릭시 리로드X
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const lis = []
+  for(let i=0; i<props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read'+t.id} onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode(Number(event.target.id));
+      }}>{t.title}</a></li>)
+  }
+  return(
+    <nav>
+    <ol>
+      {lis}
+    </ol>
+    </nav>
+  );
+}
+function Create(props) {
+  return(
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={event=>{
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title,body);
+      }}>
+        <p><input type="text" name="title" placeholder='title'/></p>
+        <p><textarea name="body" placeholder='body'/></p>
+        <p><input type="submit" value="create"/></p>
+      </form>
+    </article>
+  )
+}
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
+    {id:1, title:'html', body:'html is...'},
+    {id:2, title:'css', body:'css is...'},
+    {id:3, title:'javascript', body:'javascript is...'}
+  ]);
+
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, Web"/>
+  } else if (mode === 'READ') {
+    let title, body = null;
+    for(let i=0; i<topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}/>
+  } else if(mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body)=> {
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+    }}></Create>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header title="WEB" onChangeMode={()=>{
+      setMode('WELCOME');
+    }}/>
+    <Nav topics={topics} onChangeMode={(_id)=>{
+      setMode('READ');
+      setId(_id);
+    }}/>
+    {content}
+    <a href="/create" onClick={event=>{
+      event.preventDefault();
+      setMode('CREATE');
+    }}>Create</a>
     </div>
   );
 }
